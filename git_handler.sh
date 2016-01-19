8b1317643c42beef2d06acbdf3d5b01deabfb2b8
Jason Kulatunga <jk17@ualberta.ca>
2016-01-19 10:12:30 -0800

    successfully call sh script via fastcgi.

--- git_handler.sh
+++ git_handler.sh
@@ -1,5 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # -*- coding: utf-8 -*-
+
+#check if the requested git repository exists.
+
+
 echo "Content-type:text/html\r\n"
 echo "<html><head>"
 echo "<title>test</title>"
@@ -8,4 +12,5 @@ echo '<meta name="keywords" content="test">'
 echo '<meta http-equiv="Content-type" content="text/html;charset=UTF-8">'
 echo '<meta name="ROBOTS" content="noindex">'
 echo "</head><body><pre>"
+env
 echo "</pre></body></html>"
--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ echo "Starting fcgiwrap service..."
 service fcgiwrap start
 
 echo "Starting nginx daemon..."
-nginx -g "daemon off;"
+nginx #-g "daemon off;"
 
-#tail -f /var/log/nginx/error.log -f /var/log/nginx/access.log
+tail -f /var/log/nginx/error.log -f /var/log/nginx/access.log -f /var/log/nginx/https.git.gitmask.com.log
