a965b78e40a800d392f29d0ba8ca318f8add220e
Jason Kulatunga <jk17@ualberta.ca>
2016-01-21 10:53:44 -0800

    only enable the https endpoint after the letencrypt ssl certs have been generated.

--- start.sh
+++ start.sh
@@ -8,4 +8,14 @@ service fcgiwrap start
 echo "Starting nginx daemon..."
 nginx #-g "daemon off;"
 
+echo "Enable the http endpoint"
+ln -s /etc/nginx/sites-available/http.gitmask.conf /etc/nginx/sites-enabled/http.gitmask.conf
+
+echo "Generate Letsencrypt SSL certificates"
+cd "/srv/letsencrypt/" && ./letsencrypt.sh --cron
+
+echo "Enable the https endpoint"
+ln -s /etc/nginx/sites-available/https.gitmask.conf /etc/nginx/sites-enabled/https.gitmask.conf
+
+
 tail -f /var/log/nginx/error.log -f /var/log/nginx/access.log -f /var/log/nginx/https.git.gitmask.com.log -f /var/log/nginx/http.git.gitmask.com.log
